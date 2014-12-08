c08a5ca74c4888ed6928340320ff459d1909c9c1
Luke Sandell <lasandell@gmail.com>
2014-12-07 20:53:08 -0800

    Add test solution to make debugging easier

---
+++ tests/FSharp.Interop.ComProvider.Tests/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+
+type Shell = COM.``Microsoft Shell Controls And Automation``.``1.0``
+
+[<EntryPoint; STAThread>]
+let main argv =
+    let shell = Shell.ShellClass()
+    shell.Open @"C:\"
+    0
---
+++ tests/FSharp.Interop.ComProvider.Tests/Script.fsx
@@ -0,0 +1,7 @@
+﻿#I "../../bin"
+#r "FSharp.Interop.ComProvider.dll"
+
+type Shell = COM.``Microsoft Shell Controls And Automation``.``1.0``
+
+let shell = Shell.ShellClass()
+shell.Open @"C:\"
